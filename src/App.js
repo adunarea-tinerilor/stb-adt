2438cd0f25f8c042047b3106e8031067089d6926
Alina <alina.nanestean@usource.ro>
2020-07-06 15:26:29 +0300

    design updates

--- src/App.js
+++ src/App.js
@@ -66,15 +66,15 @@ export default class App extends React.Component {
         {
           date: "02.07.2020",
           subjectLabel: "Întrebări discutate:",
-          questionList: ["Definiţi cuvântul ordine, care a fost numită \"prima lege a cerului\", şi arătaţi cum răsplăţile şi pedepsele sunt însoţitoarele necesare ale Legii Iubirii într-un cămin ideal. Vol. 6 pag. 525 p. 1, 2 [ 23:22 ]",
-            "Cum să aibă grijă părinţii atunci când corectează pe copii prin cuvânt sau faptă? Vol. 6 pag. 526 p. 1 [ 1:47:00 ]"],
+          questionList: ["[ 23:22 ] Definiţi cuvântul ordine, care a fost numită \"prima lege a cerului\", şi arătaţi cum răsplăţile şi pedepsele sunt însoţitoarele necesare ale Legii Iubirii într-un cămin ideal. (Vol. 6 pag. 525 p. 1,2)",
+            "[ 1:47:00 ] Cum să aibă grijă părinţii atunci când corectează pe copii prin cuvânt sau faptă? (Vol. 6 pag. 526 p. 1)"],
           videoSrc: "https://www.youtube.com/watch?v=Sela3kKs8ng"
         },
         {
           date: "25.06.2020",
           subjectLabel: "Întrebări discutate:",
-          questionList: ["Program copii - poezii & cântări [ 18:52 ]",
-            "Cum trebuie conduse căminele Noilor Creaţii şi când şi cum trebuie folosită nuiaua dacă este necesar? Vol. 6 pag. 524 p. 1, 2 [ 1:17:03 ]"],
+          questionList: ["[ 18:52 ] Program copii - poezii & cântări",
+            "[ 1:17:03 ] Cum trebuie conduse căminele Noilor Creaţii şi când şi cum trebuie folosită nuiaua dacă este necesar? (Vol. 6 pag. 524 p. 1,2)"],
           videoSrc: "https://www.youtube.com/watch?v=Xtt-TKpBGkc"
         },
         {
@@ -86,36 +86,28 @@ export default class App extends React.Component {
         {
           subjectLabel: "Întrebări discutate:",
           date: "11.06.2020",
-          questionList: [
-            "Cum trebuie să avem grijă de hrana și sănătatea copiilor noștri? Vol. 6 pag. 522 - 523 [ 23:44 ]"
-          ],
+          questionList: ["[ 23:44 ] Cum trebuie să avem grijă de hrana și sănătatea copiilor noștri? (Vol. 6 pag. 522 - 523)"],
           videoSrc: "https://m.youtube.com/watch?v=2Y8YjhPFXj4"
         },
         {
           subjectLabel: "Întrebări discutate:",
           date: "4.06.2020",
-          questionList: [
-            "Cum putem influența, educa, crește copii mai avantajos în condițiile imperfecte din prezent? [ 19:20 ]"
-          ],
+          questionList: ["[ 19:20 ] Cum putem influența, educa, crește copii mai avantajos în condițiile imperfecte din prezent?"],
           videoSrc: "https://www.youtube.com/watch?v=m_HWlMmS8EI&feature=emb_title"
         },
         {
           subjectLabel: "Întrebări discutate:",
           date: "28.05.2020",
-          questionList: [
-            "Program copii - poezii & cântări [ 10:32 ]",
-            "Rolul reproducerii în viața omenirii și a Noii creații? - Vol. 6 pag. 520 par. 2 [ 1:09:54 ]",
-            "Responsabilitatea noastră și a Domnului în creșterea urmașilor."
-          ],
+          questionList: ["[ 10:32 ] Program copii - poezii & cântări",
+            "[ 1:09:54 ] Rolul reproducerii în viața omenirii și a Noii creații? (Vol. 6 pag. 520 par. 2)",
+            "Responsabilitatea noastră și a Domnului în creșterea urmașilor."],
           videoSrc: "https://www.youtube.com/watch?v=Tna0fxadpNA&feature=emb_title"
         },
         {
           subjectLabel: "Întrebări discutate:",
           date: "21.05.2020",
-          questionList: [
-            "Decizia de a avea copii sau nu. [ 13:54 ]",
-            "Câți copii să avem? [ 1:34:18 ]"
-          ],
+          questionList: ["[ 13:54 ] Decizia de a avea copii sau nu.",
+            "[ 1:34:18 ] Câți copii să avem?"],
           videoSrc: "https://www.youtube.com/watch?v=gnwQ7HpVqR0&feature=emb_title"
         }
       ],
