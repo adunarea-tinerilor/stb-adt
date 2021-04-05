564d31c1b6a2dd61cf25fe2bb673d594fbcd27f2
adunarea-tinerilor <adunarea.tinerilor@gmail.com>
2021-04-05 09:47:28 +0300

    weekley changes

--- src/App.js
+++ src/App.js
@@ -17,38 +17,26 @@ export default class App extends React.Component {
 
     this.state = {
       // SITE UPDATE DATE:
-      footerUpdateDate: "17 Decembrie 2020",
+      footerUpdateDate: "5 Aprilie 2021",
 
       // Meeting TIME Link:
-      meetingHour: "https://www.timeanddate.com/worldclock/fixedtime.html?msg=%C3%8Ent%C3%A2lnirea+Tinerilor+de+Joi&iso=20201217T10&p1=202&ah=2&am=30",
+      meetingHour: "https://www.timeanddate.com/worldclock/fixedtime.html?msg=%C3%8Ent%C3%A2lnirea+Tinerilor+de+Joi&iso=20210107T10&p1=202&ah=2&am=30",
 
       // NEXT MEETING Details:
-      nextMeetingDate: "Joi, 24 Decembrie",
+      nextMeetingDate: "Joi, 8 Aprilie",
       // nextMeetingSubject: "Adunare de Expreriențe",
 
       nextMeetingSubject: "Discuții Studiu",
       nextDiscussedLinkText: "Volumul 6, Obligațiile Părintești ale Noii Creații",
-      nextDiscussedLink: "http://www.acsib.com/HTDB/Volume6/V6C13.htm#z0009380",
+      nextDiscussedLink: "http://www.acsib.com/HTDB/Volume6/V6C13.htm#z0009396",
       nextMeetingQuestions: [
-        "Din ce punct de vedere trebuie administrată mustrarea în cazul neascultării din partea copilului? pag. 553 p. 1",
-        "Care trebuie să fie standardul în toate sugestiile? pag. 554 p. 1",
-        "Poate Noua Creatură să se aştepte în mod rezonabil la o protecţie miraculoasă pentru copiii săi în marele timp de strâmtorare? Pag. 554 p. 2",
-        "Ce promisiune scripturală pare să se aplice atât la copii cât şi la toţi cei care iubesc dreptatea? pag. 555 p. 1",
-        "Care este neliniştea naturală a părinţilor pentru copiii lor în această perioadă şi cum trebuie ea învinsă? pag. 555 p. 2",
-        "Explicaţi care este cea mai bună pregătire posibilă ce poate să o facă o Nouă Creatură pentru protejarea copiilor săi în timpul de strâmtorare. pag. 556 p. 1",
-        "Care trebuie să fie atitudinea Noii Creaturi faţă de distracţiile pentru copiii săi? pag. 556 p. 2",
-
-        // "Se aplică sfatul apostolic cu privire la căsătoria Noilor Creaţii şi pentru copiii lor neconsacraţi? pag. 557 p. 1",
-        // "Cum greşesc mulţi din Noua Creaţie în această privinţă? pag. 557 p. 2, 3, 4",
-        // "La ce vârstă ar părea potrivit ca bărbaţii şi respectiv femeile să se căsătorească? pag. 558 p. 1",
-        // "Cum pot ajuta nişte părinţi înţelepţi pe copiii lor în găsirea partenerului potrivit? pag. 558 p. 2",
-        // "Care este legătura strânsă între mintea curată şi sănătoasă şi corpul curat şi sănătos? pag. 559 p. 1",
-        // "În acest scop, cum ar trebui să procedeze părintele în privinţa aerisirii, curăţeniei şi a exerciţiului mintal şi fizic al copiilor lui? pag. 559 p. 2",
-        // "În care trei categorii se pot împărţi alimentele? Şi care este proporţia potrivită din fiecare pentru hrănire pe durata unei zile? pag. 560 p. 1-4",
-        // "Cum ar putea fi aranjată satisfăcător o dietă vegetariană dacă ar fi nevoie să facem economie? pag. 561 p. 1",
-        // "Explicaţi rezultatele dăunătoare a unei diete disproporţionate în special a hranei ce conţine amidon. pag. 561 p. 2",
-        // "Să avem grijă ca să nu facem din dietă o manie”? pag. 561 p. 3",
-        // "De ce este o discuţie veselă şi folositoare un însoţitor de dorit la masa familiei? pag. 562 p. 1"
+        "Care este legătura strânsă între mintea curată şi sănătoasă şi corpul curat şi sănătos? pag. 559 p. 1",
+        "În acest scop, cum ar trebui să procedeze părintele în privinţa aerisirii, curăţeniei şi a exerciţiului mintal şi fizic al copiilor lui? pag. 559 p. 2",
+        "În care trei categorii se pot împărţi alimentele? Şi care este proporţia potrivită din fiecare pentru hrănire pe durata unei zile? pag. 560 p. 1-4",
+        "Cum ar putea fi aranjată satisfăcător o dietă vegetariană dacă ar fi nevoie să facem economie? pag. 561 p. 1",
+        "Explicaţi rezultatele dăunătoare a unei diete disproporţionate în special a hranei ce conţine amidon. pag. 561 p. 2",
+        "Să avem grijă ca să nu facem din dietă o manie”? pag. 561 p. 3",
+        "De ce este o discuţie veselă şi folositoare un însoţitor de dorit la masa familiei? pag. 562 p. 1"
       ],
 
       // ANNOUNCEMENTS:
