104b1accb1de1f3b1d57b0a7defa007a21cd1f93
adunarea-tinerilor <adunarea.tinerilor@gmail.com>
2021-04-05 10:00:27 +0300

    Updates

---
+++ precache-manifest.2c946c66b35d49399efabe706ae03438.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5cf7aa363352c56f504d86d926d8f5c",
+    "url": "/adunarea-tinerilor/stb-adt/index.html"
+  },
+  {
+    "revision": "3f61cf072a93949c8d56",
+    "url": "/adunarea-tinerilor/stb-adt/static/css/main.ec304789.chunk.css"
+  },
+  {
+    "revision": "0dabf321004f49f99d69",
+    "url": "/adunarea-tinerilor/stb-adt/static/js/2.d3f3d49d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/adunarea-tinerilor/stb-adt/static/js/2.d3f3d49d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f61cf072a93949c8d56",
+    "url": "/adunarea-tinerilor/stb-adt/static/js/main.c8fe6ed7.chunk.js"
+  },
+  {
+    "revision": "341a9c4e12594345513b",
+    "url": "/adunarea-tinerilor/stb-adt/static/js/runtime-main.7eea6256.js"
+  },
+  {
+    "revision": "2fe9ae31b694a77609e4a52c611dc4ef",
+    "url": "/adunarea-tinerilor/stb-adt/static/media/CC-logo.2fe9ae31.png"
+  },
+  {
+    "revision": "a8e0a7ab7b241861df44ccbb6faea34d",
+    "url": "/adunarea-tinerilor/stb-adt/static/media/banned-bible-cover.a8e0a7ab.jpg"
+  }
+]);
